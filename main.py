d503613ae01645f1767d4f7c7b2048c9266a85b7
tigerFish29 <paulwchabz28@gmail.com>
2022-02-13 23:18:47 +0000

    Added more paths to the API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -20,6 +20,20 @@ class Post(BaseModel):
 my_posts = [{"title": "title of the post", "content": "content of post 1", "id": 1}]
 
 
+# find the posts [id]
+def find_posts(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
+# get the index of a post
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World hope you are good"}
@@ -32,9 +46,42 @@ def get_posts():
 
 # title should be string content => should be string
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Post):
     post_dict = new_post.dict()
     post_dict['id'] = randrange(0, 500)
     my_posts.append(post_dict)
     return {"new post": post_dict}
+
+
+# get a post
+@app.get("/post/{id}")
+def get_post(id: int, response: Response):
+    post = find_posts(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return {"post_detail": post}
+
+
+# delete a post
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item does not exit!")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# update the post
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"message": "updated schemas"}
+
+
